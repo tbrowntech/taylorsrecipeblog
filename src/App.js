b5647b2afc84d3584d22875612dd365083f18c36
Taylor Brown <ataylorbrown91@gmail.com>
2024-07-16 17:39:46 -0600

    blog: adding first draft of page in react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import jetski from './jetski.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={jetski} className='jetSki-pic' alt='jetski'/>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SeaDoo Wake Pro.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Google
         </a>
       </header>
     </div>
